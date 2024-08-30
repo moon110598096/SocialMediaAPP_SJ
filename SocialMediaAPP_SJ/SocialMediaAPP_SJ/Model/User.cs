bd28c0257336067a8e6d4ceab855ce633aae4177
moon110598096 <19980405@gmail.com>
2024-08-30 22:13:47 +0800

    [Add] Added database column mapping annotations to User entity

--- SocialMediaAPP_SJ/SocialMediaAPP_SJ/Model/User.cs
+++ SocialMediaAPP_SJ/SocialMediaAPP_SJ/Model/User.cs
@@ -1,11 +1,17 @@
 ﻿using Org.BouncyCastle.Asn1.Mozilla;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SocialMediaAPP_SJ.Model
 {
     public class User
     {
+        [Key]
+        [Column("user_id")]
         public int UserId {  get; set; }
+        [Column("password")]
         public String Password { get; set; }
+        [Column("name")]
         public String Name { get; set; }
     }
 }
