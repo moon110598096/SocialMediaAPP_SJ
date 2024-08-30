d8f73fe462778b2d4de07fcfad1f040a96b1c49f
moon110598096 <19980405@gmail.com>
2024-08-30 21:46:15 +0800

    [Setup] Update fields in Comment, Post, and User entities

--- SocialMediaAPP_SJ/SocialMediaAPP_SJ/Model/Post.cs
+++ SocialMediaAPP_SJ/SocialMediaAPP_SJ/Model/Post.cs
@@ -2,5 +2,11 @@
 {
     public class Post
     {
+        public String Title { get; set; }
+        public String Content { get; set; }
+        public DateTime date { get; set; }
+        public int PostId { get; set; }
+        public string UserId { get; set; }
+        public bool Removed { get; set; }
     }
 }
--- SocialMediaAPP_SJ/SocialMediaAPP_SJ/Model/User.cs
+++ SocialMediaAPP_SJ/SocialMediaAPP_SJ/Model/User.cs
@@ -1,6 +1,11 @@
-﻿namespace SocialMediaAPP_SJ.Model
+﻿using Org.BouncyCastle.Asn1.Mozilla;
+
+namespace SocialMediaAPP_SJ.Model
 {
     public class User
     {
+        public int UserId {  get; set; }
+        public String Password { get; set; }
+        public String Name { get; set; }
     }
 }
