8f049aa1799ff09de962615c5b55d9879c367eb0
moon110598096 <19980405@gmail.com>
2024-08-30 16:53:46 +0800

    [Setup Configure database settings for initial setup

---
+++ SocialMediaAPP_SJ/SocialMediaAPP_SJ/Data/ApplicationDbContext .cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaAPP_SJ.Model;
+
+namespace SocialMediaAPP_SJ.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
