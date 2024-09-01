b33666801bc34049481a27a0fa2f90d74d810f70
moon110598096 <19980405@gmail.com>
2024-09-01 22:05:48 +0800

    [Add] Added database column mapping annotations to Comment entity

--- SocialMediaAPP_SJ/SocialMediaAPP_SJ/Model/Comment.cs
+++ SocialMediaAPP_SJ/SocialMediaAPP_SJ/Model/Comment.cs
@@ -1,14 +1,26 @@
-﻿namespace SocialMediaAPP_SJ.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SocialMediaAPP_SJ.Model
 {
     public class Comment
     {
+        [Key]
+        [Column("comment_id")]
         public String CommentId { get; set; }
+        [Column("name")]
         public String Name {  get; set; }
+        [Column("message")]
         public String Message {  get; set; }
+        [Column("ip")]
         public String Ip { get; set; }
+        [Column("date")]
         public DateTime Date { get; set; }
+        [Column("removed")]
         public bool Removed { get; set; }
+        [Column("user_id")]
         public String UserId { get; set; }
+        [Column("post_id")]
         public String PostId { get; set; } 
     }
 }
